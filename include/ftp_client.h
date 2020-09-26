54e78476136072cf9ecce09a3bd2abd9db0e6162
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-09-26 22:00:15 +0200

    Update ftp_client.h

--- include/ftp_client.h
+++ include/ftp_client.h
@@ -2,7 +2,6 @@
 #include <winsock2.h>
 
 typedef struct ftp_client_t ftp_client_t;
-typedef struct sys_socket_t sys_socket_t;
 
 struct ftp_client_t {
     char *host;
@@ -15,8 +14,8 @@ struct ftp_client_t {
 
 void socket_init(ftp_client_t *init);
 ftp_client_t *ftp_client_connect(char *host, int port, char *password, char *username);
-void ftp_mkdir(char *folders);
-int socket_connect(ftp_client_t *client);
+void ftp_mkdir(ftp_client_t *fd, char *folders);
+void socket_connect(ftp_client_t *client);
 
 
 
