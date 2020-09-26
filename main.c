edb26a6d5974477281de07cca64639dcb0b90308
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-09-26 23:54:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,11 @@
 int main(int argc, char **argv)
 {
     ftp_client_t *ftp = ftp_client_connect("127.0.0.1", 21, "test", "jean");
+    char buffer[256];
 
-    ftp_mkdir(ftp, "Wilfreed");
+    ftp_pwd(ftp, buffer, 256);    
 
+    printf("%s\n", buffer);
 
+    ftp_mkdir(ftp, "lol");
 }
