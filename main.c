7c25027774f88fec321d1dce1770367f641a3051
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-09-26 23:27:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,9 @@
 
 int main(int argc, char **argv)
 {
-    ftp_client_t *ftp = ftp_client_connect("127.0.0.1", 21, "wilfreed", "jean");
-    printf("fd -> %d\n", ftp->fd);
-    printf("host -> %s\n", ftp->host);
-    printf("user -> %s\n", ftp->user);
-    printf("pass -> %s\n", ftp->password);
-    printf("port -> %d\n", ftp->port);
+    ftp_client_t *ftp = ftp_client_connect("127.0.0.1", 21, "test", "jean");
+
+    ftp_mkdir(ftp, "Wilfreed");
+
 
 }
